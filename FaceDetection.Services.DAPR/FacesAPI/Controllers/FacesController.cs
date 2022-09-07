0e01b2475831685731b2c4b0cf59a9d478202669
ZENOTI\divyeshb <divyesh.bhartiya@outlook.com>
2022-09-08 02:22:58 +0530

    To be continued....

---
+++ FaceDetection.Services.DAPR/FacesAPI/Controllers/FacesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FacesAPI.Controllers
+{
+    // [Route("api/[controller]")]
+    [ApiController]
+    public class FacesController : ControllerBase
+    {
+    }
+}
